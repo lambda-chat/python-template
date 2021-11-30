f16f1eb88323152fb97e680c6e1da5ec70282625
lambda-chat <chat.algebrique@gmail.com>
2021-11-30 22:27:53 +0000

    change virtualenvs.create to true

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,7 +4,14 @@ PASSWORD=password
 
 # install python packages
 pip3 install poetry
-poetry config virtualenvs.create false
+
+USE_VENV=true  # should be false in future
+if [ $USE_VENV ]; then
+    poetry config virtualenvs.create true
+    poetry config virtualenvs.in-project true
+else
+    poetry config virtualenvs.create false
+fi
 poetry install
 
 # chown dist/ volume
