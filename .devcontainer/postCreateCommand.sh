fef7e47b4a21a520ef0e1f970f6132667df4dae0
lambda-chat <chat.algebrique@gmail.com>
2021-11-30 07:02:01 +0000

    install starship prompt in postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -x
 
+PASSWORD=password
+
 # install python packages
 pip3 install poetry
 poetry config virtualenvs.create false
 poetry install --no-root
 
 # change default shell to fish
-echo "password" | chsh -s $(which fish)
+echo $PASSWORD | chsh -s $(which fish)
+
+# install starship prompt 
+curl -fsSL https://starship.rs/install.sh -o starship_install.sh
+echo $PASSWORD | sudo --stdin sh starship_install.sh -y
+rm starship_install.sh
+mkdir -p ~/.config/fish
+echo "starship init fish | source" >> ~/.config/fish/config.fish
+fish -c "set -U fish_greeting"
+
+unset PASSWORD
