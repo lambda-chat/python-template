c4ac12db96946a55338394962965ea1bb3a1b1a7
lambda-chat <chat.algebrique@gmail.com>
2024-06-04 09:30:37 +0900

    Update install.sh

--- .devcontainer/local-feature/install.sh
+++ .devcontainer/local-feature/install.sh
@@ -6,9 +6,9 @@ apt-get clean
 rm -rf /var/lib/apt/lists/*
 
 # Add a non-root user `vscode` with sudo access
-ARG USERNAME=vscode
-ARG USER_UID=1000
-ARG USER_GID=$USER_UID
+USERNAME=vscode
+USER_UID=1000
+USER_GID=$USER_UID
 
 groupadd --gid $USER_GID $USERNAME
 useradd -s /bin/bash --uid $USER_UID --gid $USER_GID -m $USERNAME
